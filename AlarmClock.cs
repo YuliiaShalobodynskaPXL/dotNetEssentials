07afd5583b7ca7febf6ed1f7ddba801a5ba14fba
YuliiaShalobodynskaPXL <43844676+YuliiaShalobodynskaPXL@users.noreply.github.com>
2020-04-08 12:46:49 +0200

    Add project files.

---
+++ AlarmClock.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace oef_10._8_wekker
+{
+    public class AlarmClock
+    {
+        private DateTime AlarmTime;
+        private int _beepTimeInSeconds;
+
+        private string CurrentTime
+        {
+            get =>  $"{DateTime.Now:T}";
+        }
+
+        
+        //private DateTime AlarmTime(int hour,int minute) {
+        //    _alarmTime = new DateTime(hour, minute);
+        //}          // set??
+
+        public AlarmClock(int hours, int minutes, int seconds = 0)
+        {  
+            _beepTimeInSeconds = 10;
+            AlarmTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, hours, minutes, seconds);
+        }
+
+        public bool isAlarmPassed(DateTime alarmTime)
+        {
+            if (AlarmTime > DateTime.Now) {
+                return true;
+            } else return false;
+            
+        }
+
+        public bool ShouldStopBeeping()
+        {
+            if ((AlarmTime.AddSeconds(_beepTimeInSeconds)) >= DateTime.Now)
+            {   
+                return true;
+            } else return false;
+        }
+
+        public void Reset()
+        {
+            AlarmTime = DateTime.MinValue;
+        }
+        
+
+
+
+        //private string currentTime;
+
+        //public AlarmClock(DateTime alarmTime) {
+        //    //DateTime dateTime = new DateTime;
+        //    //var alarmTime = dateTime; 
+        //}
+
+        //public void alarm_on(DateTime alarmTime) { 
+            
+        //    if (DateTime.Now == alarmTime){
+        //        MessageBox.Show("Wake up!");
+        //    }
+        //}
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace oef_10._8_wekker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer _clockTimer = new DispatcherTimer();
+        DispatcherTimer _alarmTimer = new DispatcherTimer();
+       
+        private AlarmClock alarmTime;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _clockTimer.Start();
+            _alarmTimer.Start();
+            Console.WriteLine(alarmTime);
+
+        }
+
+        private void setAlarmButton_Click(object sender, RoutedEventArgs e)
+        {
+            //int hours = Convert.ToInt32(setHoursTextBox.Text);
+            // int min = Convert.ToInt32(setSecTextBox.Text);
+            try
+            {
+                //alarmTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, Convert.ToInt32(setHoursTextBox.Text), Convert.ToInt32(setMinTextBox.Text), Convert.ToInt32(setSecTextBox.Text));
+                alarmTime = new AlarmClock(Convert.ToInt32(hourAlarm.Text), Convert.ToInt32(minuteAlarm.Text), Convert.ToInt32(secondsAlarm.Text));
+            }
+            catch (Exception)
+            { }
+            
+        }
+    }
+}
